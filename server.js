c49fb0edd6d8b8e90e6a38d78f9e1cdee582291f
blzgokul <blackzzayn@gmail.com>
2026-01-19 17:48:49 +0530

    Add test route

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/test", (req, res) => {
+  res.send("Backend is running");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
